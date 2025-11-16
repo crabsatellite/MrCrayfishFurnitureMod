f06553bfa52f23481031cd2c8bfddfff472b0d10
Alex Lee <islicy@outlook.com>
2025-11-16 20:26:35 +0900

    Refactor TrampolineItem to utilize CustomData for block entity tag management

--- src/main/java/com/mrcrayfish/furniture/item/TrampolineItem.java
+++ src/main/java/com/mrcrayfish/furniture/item/TrampolineItem.java
@@ -3,12 +3,14 @@ package com.mrcrayfish.furniture.item;
 import com.mrcrayfish.furniture.Reference;
 import com.mrcrayfish.furniture.tileentity.TrampolineBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -27,8 +29,9 @@ public class TrampolineItem extends BlockItem implements CreativeItem {
     @Override
     protected boolean updateCustomBlockEntityTag(BlockPos pos, Level level, @Nullable Player player, ItemStack stack, BlockState state) {
         super.updateCustomBlockEntityTag(pos, level, player, stack, state);
-        CompoundTag blockEntityTag = stack.getTagElement("BlockEntityTag");
-        if (blockEntityTag != null) {
+        CustomData customData = stack.getOrDefault(DataComponents.BLOCK_ENTITY_DATA, CustomData.EMPTY);
+        if (!customData.isEmpty()) {
+            CompoundTag blockEntityTag = customData.copyTag();
             DyeColor color = DyeColor.byId(blockEntityTag.getInt("Color"));
             if (level.getBlockEntity(pos) instanceof TrampolineBlockEntity blockEntity) {
                 blockEntity.setColour(color);
@@ -39,9 +42,9 @@ public class TrampolineItem extends BlockItem implements CreativeItem {
 
     @Override
     public String getDescriptionId(ItemStack stack) {
-        CompoundTag tag = stack.getTag();
-        if (tag != null) {
-            CompoundTag blockEntityTag = tag.getCompound("BlockEntityTag");
+        CustomData customData = stack.getOrDefault(DataComponents.BLOCK_ENTITY_DATA, CustomData.EMPTY);
+        if (!customData.isEmpty()) {
+            CompoundTag blockEntityTag = customData.copyTag();
             DyeColor color = DyeColor.byId(blockEntityTag.getInt("Color"));
             return String.format("block.%s.%s_trampoline", Reference.MOD_ID, color.getName());
         }
@@ -52,11 +55,9 @@ public class TrampolineItem extends BlockItem implements CreativeItem {
     public void fill(Consumer<ItemStack> output) {
         for (DyeColor color : DyeColor.values()) {
             ItemStack stack = new ItemStack(this);
-            CompoundTag tag = new CompoundTag();
             CompoundTag blockEntityTag = new CompoundTag();
             blockEntityTag.putInt("Color", color.getId());
-            tag.put("BlockEntityTag", blockEntityTag);
-            stack.setTag(tag);
+            stack.set(DataComponents.BLOCK_ENTITY_DATA, CustomData.of(blockEntityTag));
             output.accept(stack);
         }
     }
