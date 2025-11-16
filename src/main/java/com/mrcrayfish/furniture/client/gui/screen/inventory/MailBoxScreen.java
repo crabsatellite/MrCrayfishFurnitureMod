372be59dcdeefe86b9f9520d5041b1790abd1e19
Alex Lee <islicy@outlook.com>
2025-11-16 19:27:22 +0900

    Refactor render methods in FreezerScreen, MailBoxScreen, and PostBoxScreen to include mouse coordinates for improved background rendering

--- src/main/java/com/mrcrayfish/furniture/client/gui/screen/inventory/MailBoxScreen.java
+++ src/main/java/com/mrcrayfish/furniture/client/gui/screen/inventory/MailBoxScreen.java
@@ -51,7 +51,7 @@ public class MailBoxScreen extends AbstractContainerScreen<MailBoxMenu> {
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(graphics);
+        this.renderBackground(graphics, mouseX, mouseY, partialTicks);
         super.render(graphics, mouseX, mouseY, partialTicks);
         this.renderTooltip(graphics, mouseX, mouseY);
     }
--- src/main/java/com/mrcrayfish/furniture/client/gui/screen/inventory/PostBoxScreen.java
+++ src/main/java/com/mrcrayfish/furniture/client/gui/screen/inventory/PostBoxScreen.java
@@ -147,7 +147,7 @@ public class PostBoxScreen extends AbstractContainerScreen<PostBoxMenu> {
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(graphics);
+        this.renderBackground(graphics, mouseX, mouseY, partialTicks);
         super.render(graphics, mouseX, mouseY, partialTicks);
         this.renderTooltip(graphics, mouseX, mouseY);
     }
